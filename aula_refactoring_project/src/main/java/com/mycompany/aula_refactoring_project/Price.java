bcbc2773e783b38ff71ee62986ca6a88da5917dd
marscos <mcarvalhodash@gmail.com>
2020-10-19 11:02:45 -0300

    Commit 12

--- aula_refactoring_project/src/main/java/com/mycompany/aula_refactoring_project/Price.java
+++ aula_refactoring_project/src/main/java/com/mycompany/aula_refactoring_project/Price.java
@@ -10,5 +10,29 @@ package com.mycompany.aula_refactoring_project;
  * @author marcos
  */
 public abstract class Price {
-   public abstract int getPriceCode();
+
+    public abstract int getPriceCode();
+
+    public double getCharge(int daysRented) {
+        double amount = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                amount += 2;
+                if (daysRented > 2) {
+                    amount += (daysRented - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                amount += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                amount += 1.5;
+                if (daysRented > 3) {
+                    amount += (daysRented - 3) * 1.5;
+                }
+                break;
+        }
+        return amount;
+    }
+
 }
