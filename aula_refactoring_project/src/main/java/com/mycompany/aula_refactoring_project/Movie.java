ac58e3fc2b0851bea0f640e658cbd5f98b11232c
marscos <mcarvalhodash@gmail.com>
2020-10-19 11:00:39 -0300

    Commit 11

--- aula_refactoring_project/src/main/java/com/mycompany/aula_refactoring_project/Movie.java
+++ aula_refactoring_project/src/main/java/com/mycompany/aula_refactoring_project/Movie.java
@@ -16,19 +16,31 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
 
     private String _title;
-    private int _priceCode;
+    private Price _price;
 
     public Movie(String title, int priceCode) {
         _title = title;
-        _priceCode = priceCode;
+        setPriceCode(priceCode);
     }
 
     public int getPriceCode() {
-        return _priceCode;
+        return _price.getPriceCode();
     }
 
     public void setPriceCode(int arg) {
-        _priceCode = arg;
+        switch (arg) {
+         case REGULAR:
+            _price = new RegularPrice();
+            break;
+         case CHILDRENS:
+            _price = new ChildrensPrice();
+            break;
+         case NEW_RELEASE:
+            _price = new NewReleasePrice();
+            break;
+         default:
+            throw new IllegalArgumentException("Incorrect Price Code");
+      }
     }
 
     public String getTitle() {
@@ -37,7 +49,7 @@ public class Movie {
 
     public double getCharge(int daysRented) {
         double amount = 0;
-        switch (this._priceCode) {
+        switch (getPriceCode()) {
             case Movie.REGULAR:
                 amount += 2;
                 if (daysRented > 2) {
@@ -59,7 +71,7 @@ public class Movie {
 
     public int getFrequentRenterPoints(int daysRented) {
         // add bonus for a two day new release rental
-        if ((this._priceCode == Movie.NEW_RELEASE)
+        if ((getPriceCode() == Movie.NEW_RELEASE)
                 && daysRented > 1) {
             return 2;
         }
---
+++ aula_refactoring_project/src/main/java/com/mycompany/aula_refactoring_project/NewReleasePrice.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.aula_refactoring_project;
+
+/**
+ *
+ * @author marcos
+ */
+public class NewReleasePrice extends Price {
+   public int getPriceCode() {
+       return Movie.NEW_RELEASE;
+   }
+}
