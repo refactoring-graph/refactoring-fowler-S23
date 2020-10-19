f8f7d9c6c73b1507e6273906df70d9d9b22a5d44
marscos <mcarvalhodash@gmail.com>
2020-10-19 11:07:51 -0300

    Commit 15

--- aula_refactoring_project/src/main/java/com/mycompany/aula_refactoring_project/NewReleasePrice.java
+++ aula_refactoring_project/src/main/java/com/mycompany/aula_refactoring_project/NewReleasePrice.java
@@ -19,4 +19,8 @@ public class NewReleasePrice extends Price {
         return daysRented * 3;
     }
 
+    public int getFrequentRenterPoints(int daysRented) {
+        return (daysRented > 1) ? 2 : 1;
+    }
+
 }
